cd589a6c34a31c97d03a5cd64ef16e5fa1ae95d3
Jon Gunter <jon@jongunter.com>
2017-10-22 17:00:58 -0600

    Sample code

---
+++ test.hs
@@ -0,0 +1,8 @@
+-- http://learnyouahaskell.com/starting-out
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+conanO'brien = "Its me!!!"
